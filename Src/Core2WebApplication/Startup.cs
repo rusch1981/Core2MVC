545a2f03de7445f054de136aafa7ba97074d870b
arusch <arusch@geninfo.com>
2018-03-11 11:45:30 -0400

    Added Mvc Service and Mvc Default Route. Added basic home controller with Index Added basic Index View

--- Src/Core2WebApplication/Startup.cs
+++ Src/Core2WebApplication/Startup.cs
@@ -15,20 +15,14 @@ namespace Core2WebApplication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //Adds MVC to the IApplicationBuilder request execution pipeline with a default route named 'default' and the following template: '{controller=Home}/{action=Index}/{id?}'.
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
